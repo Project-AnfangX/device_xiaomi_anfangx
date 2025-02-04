88271a2f3c50c3e1771702c699462e2716e72b56
uwu-gl <moegluwu0@gmail.com>
2025-02-05 00:50:41 +0800

    anfangx: RisingOS bringup

--- lineage_anfangx.mk
+++ lineage_anfangx.mk
@@ -17,7 +17,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_anfangx
 PRODUCT_DEVICE := anfangx
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := tucana
+PRODUCT_MODEL := IF 1
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
@@ -26,3 +26,13 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="tucana-user 11 RKQ1.200826.002 V13.0.2.0.RFDMIXM release-keys"
 
 BUILD_FINGERPRINT := Xiaomi/tucana/tucana:11/RKQ1.200826.002/V13.0.2.0.RFDMIXM:user/release-keys
+
+# RisingOS Config
+RISING_MAINTAINER="uwugl"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Qualcomm Snapdragon 8 Gen 114514" \
+    RisingMaintainer="uwugl"
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := false
+WITH_GMS := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+RISING_PACKAGE_TYPE := GAPPS
